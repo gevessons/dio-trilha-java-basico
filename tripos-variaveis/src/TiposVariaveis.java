23cd9970aca9d14612a4cac31dab3e213ea52c9a
gevessons <gevesson_silva@hotmail.com>
2024-10-31 20:05:49 -0300

    Iniciando tipos de variáveis

--- tripos-variaveis/src/TiposVariaveis.java
+++ tripos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,6 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 5;
+        System.out.println(numero);
     }
 }
