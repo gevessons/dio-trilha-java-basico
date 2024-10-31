6c0176076ca351a9ace0389fdccae76f1a4458bb
gevessons <gevesson_silva@hotmail.com>
2024-10-31 20:46:25 -0300

    Aprendendo sobre operadores de comparação

--- tripos-variaveis/src/TiposVariaveis.java
+++ tripos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,24 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        int numero = 5;
-        System.out.println(numero);
+
+        String nomeUm = "GEVESSON";
+        String nomeDois = new String("GEVESSON");
+
+        System.out.println(nomeUm == nomeDois); // COMPARAÇÃO ENTRE VARIAVEIS
+        System.out.println(nomeUm.equals(nomeDois)); // COMPARAÇÃO ENTRE OBJETOS E STRING
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        System.out.println(simNao);
+
+        simNao = numero1 != numero2;
+        System.out.println(simNao);
+        
+        simNao = numero1 > numero2;
+        System.out.println(simNao);
+    
     }
 }
